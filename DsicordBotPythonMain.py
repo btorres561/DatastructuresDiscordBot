ff38c26eb711d292842c09da0e9e0f7b7efce2d2
btorres561 <briantorres1861@gmail.com>
2025-05-10 17:53:05 -0400

    Added Comments
    Added comments to each of the blocks of code.

--- DsicordBotPythonMain.py
+++ DsicordBotPythonMain.py
@@ -3,19 +3,26 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv    
 
+#Retrieving the discord Bot Token, Learned from ChatGpt
 load_dotenv()
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
 
+#Allow the bot to read messages, Learned my friend Mitchel
 intents = discord.Intents.default()
 intents.message_content = True
+
+#Allows bot commands, specifies the character required to call commands, Learned from ChatGpt
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+#Prints a message after the bot successfully connects to Discord, Learned from ChatGpt
 @bot.event
 async def onReady():
     print(f'logged in as {bot.user}')
-
+    
+#Defines the commands for the bot, Learned from ChatGpt
 @bot.command()
-async def hello(ctx):
+async def hello(ctx): #Defines the hello command, when user calls $hello the bot will send a message saying Hello!
     await ctx.send('Hello!')
 
+#Runs the bot
 bot.run(TOKEN)
