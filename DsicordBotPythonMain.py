e2e40c6684ed83ff6f309295407883c61d59c42e
btorres561 <briantorres1861@gmail.com>
2025-05-10 17:40:52 -0400

    Discord Implementation
    Implementing discord bot functionality. Also implementing a .env file to protect the discord bot Token as having it hardcoded triggers a security error.

--- DsicordBotPythonMain.py
+++ DsicordBotPythonMain.py
@@ -1,4 +1,21 @@
-def main():
-    print("Hello World")
-    
-main()
+import discord
+from discord.ext import commands
+import os
+from dotenv import load_dotenv    
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def onReady():
+    print(f'logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Hello!')
+
+bot.run(TOKEN)
