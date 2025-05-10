fc695492d94cece468095eca2f6d149be8a518a0
btorres561 <briantorres1861@gmail.com>
2025-05-10 19:06:30 -0400

    Added banned word list and on_message function
    The on_message function monitors messages sent by users and strips them of special characters and splits them into a list of words. Each word in the list is compared with the bannedWords list and if a banned word is found it prints a warning message.

--- DsicordBotPythonMain.py
+++ DsicordBotPythonMain.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
 import os
-from dotenv import load_dotenv    
+from dotenv import load_dotenv
+import re
 
 #Retrieving the discord Bot Token, Learned from ChatGpt
 load_dotenv()
@@ -14,10 +15,29 @@ intents.message_content = True
 #Allows bot commands, specifies the character required to call commands, Learned from ChatGpt
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-#Prints a message after the bot successfully connects to Discord, Learned from ChatGpt
+#Defines list of banned words
+bannedWords = ['hi', 'what', 'guh',]
+
+#dictionary of misbehaving users and their offenses
+badUser = {}
+
+#Defines the bots functions in response to events
 @bot.event
-async def onReady():
+async def onReady(): #Prints a message after the bot successfully connects to Discord, Learned from ChatGpt
     print(f'logged in as {bot.user}')
+
+@bot.event
+async def on_message(message): #Scans messages sent by users for banned words and if a banned word is found a warning message is sent
+    if message.author == bot.user: #Prevents bot from responding to itself
+        return
+    
+    words = re.sub(r"[^a-zA-Z0-9\s]", "", message.content.lower()).split(" ")
+    
+    for word in words:
+        if word in bannedWords:
+            await message.channel.send(f'erm "{word}" is a bad word, you cant say that.')
+    await bot.process_commands(message) #Allows bot to process commands still
+
     
 #Defines the commands for the bot, Learned from ChatGpt
 @bot.command()
